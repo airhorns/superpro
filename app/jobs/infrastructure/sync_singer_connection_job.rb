3680edd17035a648d893a710a828c19f2c008c4f
Harry Brundage <harry.brundage@gmail.com>
2019-08-06 22:12:53 -0400

    Don't retry sync jobs for now, let them retry on the periodic schedule

--- app/jobs/infrastructure/sync_singer_connection_job.rb
+++ app/jobs/infrastructure/sync_singer_connection_job.rb
@@ -1,4 +1,6 @@
 class Infrastructure::SyncSingerConnectionJob < Que::Job
+  self.maximum_retry_count = 0
+
   def run(connection_id:)
     connection = Connection.find(connection_id)
     Infrastructure::SingerConnectionSync.new(connection.account).sync(connection)
