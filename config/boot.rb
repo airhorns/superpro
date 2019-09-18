389c8d6040efb8f1b2dc7247f98d53e4a0b161b8
Harry Brundage <harry.brundage@gmail.com>
2019-09-18 10:37:04 -0400

    See if this fixes circle

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,8 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Debug an issue on circle where trestle admin names were coming out as mangled strings
+unless ENV["CI"]
+  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+end
