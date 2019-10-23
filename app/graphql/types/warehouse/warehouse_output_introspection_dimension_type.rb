7507164dcf06f0ef5d1f031453d28385ab553732
Harry Brundage <harry.brundage@gmail.com>
2019-10-23 18:23:09 -0400

    Format truncated dates nicely

--- app/graphql/types/warehouse/warehouse_output_introspection_dimension_type.rb
+++ app/graphql/types/warehouse/warehouse_output_introspection_dimension_type.rb
@@ -3,6 +3,7 @@
 class Types::Warehouse::WarehouseOutputIntrospectionDimensionType < Types::BaseObject
   field :id, String, null: false
   field :data_type, Types::Warehouse::WarehouseDataTypeEnum, null: false
+  field :operator, String, null: true
   field :label, String, null: false
   field :sortable, Boolean, null: false
 end
