e378fc2fbd25229834145ef7a5a64e9e155beb7a
Harry Brundage <harry.brundage@gmail.com>
2019-11-05 11:23:04 -0500

    Add inplace markdown editing for sanity

--- app/javascript/app/components/superviz/components/render/MarkdownBlockRenderer.tsx
+++ app/javascript/app/components/superviz/components/render/MarkdownBlockRenderer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Box, Markdown } from "grommet";
+import { Markdown, TextArea, Box } from "grommet";
 import styled from "styled-components";
 import { MarkdownBlock } from "../../schema";
+import { SimpleModal } from "superlib";
 
 export const StyledMarkdownBlockContainer = styled.div`
   @media print {
@@ -9,10 +10,21 @@ export const StyledMarkdownBlockContainer = styled.div`
   }
 `;
 
+export const HackyMarkdownEditor = (props: { block: MarkdownBlock }) => {
+  const [value, setValue] = React.useState(props.block.markdown);
+  return (
+    <SimpleModal trigger={setShow => <Markdown onClick={() => setShow(true)}>{value}</Markdown>}>
+      <Box width="xlarge" height="xlarge">
+        <TextArea fill value={value} onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setValue(event.target.value)} />
+      </Box>
+    </SimpleModal>
+  );
+};
+
 export const MarkdownBlockRenderer = (props: { block: MarkdownBlock }) => {
   return (
     <StyledMarkdownBlockContainer>
-      <Markdown>{props.block.markdown}</Markdown>
+      <HackyMarkdownEditor block={props.block} />
     </StyledMarkdownBlockContainer>
   );
 };
